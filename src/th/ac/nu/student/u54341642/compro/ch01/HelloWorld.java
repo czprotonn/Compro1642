78d25d4099aced77f84e7b03138a3389e1be92b1
Chalisa <bp_clash@hotmail.com>
2012-06-19 14:37:55 +0700

    สร้าง HelloWorld

---
+++ src/th/ac/nu/student/u54341642/compro/ch01/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341642.compro.ch01;
+
+/**
+ *
+ * @author Itstationnu
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello,World!");
+        
+    }
+}
