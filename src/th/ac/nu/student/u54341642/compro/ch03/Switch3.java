92f14cd6acf1f013e25967142cafdc36825fd774
Chalisa <bp_clash@hotmail.com>
2012-07-15 12:46:29 +0700

    Code from slide chapter 3

---
+++ src/th/ac/nu/student/u54341642/compro/ch03/Switch3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54341642.compro.ch03;
+
+ import java.util.Scanner;
+ 
+ public class Switch3 {
+ 
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+         char grade = in.next().charAt(0);
+         switch (grade) {
+             case 'A':
+             case 'B':
+             case 'C':
+                 System.out.println("Passes");
+                 break;
+             case 'D':
+             case 'F':
+                 System.out.println("Fail");
+                 break;
+             default:
+                 System.out.println("Invalid");
+                 break;
+         }
+     }
+ }
